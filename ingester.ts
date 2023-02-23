df2dc3147c1bcdd527557792ea8217e8fcf44d5a
ilmoi <iljamoi@protonmail.com>
2023-02-22 21:45:49 -0500

    scaffold

---
+++ ingester.ts
@@ -0,0 +1,17 @@
+import {Connection, PublicKey} from '@solana/web3.js';
+
+require('dotenv').config();
+
+(async () => {
+  console.log('Ingester reporting to duty 🫡')
+
+  const rpc = process.env.RPC_PROVIDER;
+  if (!rpc) return;
+
+  const connection = new Connection(rpc);
+  const marketplaceAddress = new PublicKey("M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K");
+  const sigs = await connection.getSignaturesForAddress(marketplaceAddress)
+
+  console.log(`Looks like I fetched ${sigs.length} sigs.`)
+  console.log(`Here's what one of them looks like: ${JSON.stringify(sigs[0], null, 4)}`)
+})()
